050bf55ec6a1763133c3dd5d943754ec338f87cd
arielCaro <arielcaro.inostroza@gmail.com>
2024-11-07 11:39:10 -0300

    update new models

--- projects/api.py
+++ projects/api.py
@@ -1,8 +1,28 @@
-from projects.models import project
+from .models import project, user, role, company, session
 from rest_framework import viewsets, permissions
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, UserSerializer, RoleSerializer, CompanySerializer, SessionSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = project.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ProjectSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = user.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = role.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RoleSerializer
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = company.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = CompanySerializer
+
+class SessionViewSet(viewsets.ModelViewSet):
+    queryset = session.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SessionSerializer
--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,10 @@
 from rest_framework import routers
-from .api import ProjectViewSet
+from .api import ProjectViewSet, UserViewSet, RoleViewSet, CompanyViewSet, SessionViewSet
 router = routers.DefaultRouter()
 
 router.register('api/projects', ProjectViewSet, 'projects')
+router.register('api/users', UserViewSet, 'users')
+router.register('api/roles', RoleViewSet, 'roles')
+router.register('api/companies', CompanyViewSet, 'companies')
+router.register('api/sessions', SessionViewSet, 'sessions')
 urlpatterns = router.urls
